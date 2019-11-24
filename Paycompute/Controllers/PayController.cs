9f046768fcfe51dc5bca0b858b04770dddb9e988
Vinh Hoang <nhuvinh.hoang@gmail.com>
2019-11-24 09:04:38 +0700

    Add PaymentRecordIndexViewModel

---
+++ Paycompute/Controllers/PayController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Paycompute.Services;
+
+namespace Paycompute.Controllers
+{
+    public class PayController : Controller
+    {
+								private IPayComputationService _payComputationServie;
+
+								public PayController(IPayComputationService payComputationService)
+								{
+												_payComputationServie = payComputationService;
+								}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
